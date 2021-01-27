84bb8cf8b3bc0c894ab2b5a0069788ece8dcd9f5
Abhishek Kumar <readabhishek@gmail.com>
2021-01-26 23:12:11 -0500

    Updated 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
 
-  const [btnColor, setBtnColor] = useState('red');
-  const newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+    const [btnColor, setBtnColor] = useState('red');
+    const newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+    const [checked, setChecked] = useState(false);
 
-  return (
-      <div id='mainDiv'>
-        <button id='btn' style={{backgroundColor: btnColor, color:'white'}}
-                onClick={() => {setBtnColor(newBtnColor)}}>Click to change color to {newBtnColor}</button>
-      </div>
-  );
+
+    function setCheckedValue() {
+        let val = document.getElementById('chk1').checked;
+        setChecked(val);
+        //console.log("CheckBox Checked Element: ", val);
+    }
+
+    useEffect( () => {       /* This is React hook, combination of ComponentDidMount and ComponentDidUpdate   */
+        //console.log("CheckBox Checked State: ", checked);
+    })
+
+    return (
+        <div id='mainDiv' className="content">
+            <button id='btn' style={{backgroundColor: btnColor}}
+                    onClick={() => {setBtnColor(newBtnColor)}} disabled={checked}>Click to change color to {newBtnColor} </button>
+            <div className="check-box">
+                <input
+                    type="checkbox" id="chk1" name="chk1" value="false" onChange={() => {setCheckedValue()}}/>
+                <label htmlFor="chk1">Disable Button</label>
+            </div>
+        </div>
+);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -16,3 +16,39 @@ test('Test Button Color as Blue after Click', () => {
   expect(ColorButton).toHaveStyle({backgroundColor: 'blue'});
 });
 
+
+/* Script for Check box and Button initial condition */
+test("Check the initial conditions for Check box", () => {
+
+  /* Checks that button is enabled initially */
+  render(<App />);
+  const colorButton = screen.getByRole('button', {name: 'Click to change color to blue'});
+  expect(colorButton).toBeEnabled();
+
+  /* Checks that check-box is un-checked initially */
+  const checkBox = screen.getByRole('checkbox', {name: 'Disable Button'});
+  expect(checkBox).not.toBeChecked();
+
+});
+
+
+
+/* Script to test if button enables and disables  */
+test("Check if button disables after check-box checked and vice-versa", () => {
+  render(<App />);
+
+
+  const checkBox = screen.getByRole('checkbox', {name: 'Disable Button'});
+  const colorButton = screen.getByRole('button', {name: 'Click to change color to blue'});
+
+  /* Fire events - Make the check-box checked */
+  fireEvent.click(checkBox);
+  expect(checkBox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+
+  /* Fire events - Make the check-box un-checked */
+  fireEvent.click(checkBox);
+  expect(checkBox).not.toBeChecked();
+  expect(colorButton).not.toBeDisabled();
+
+});
