694d5c5efacf81dc6af9643c4c0c70cd5eb127f2
Abhishek Kumar <readabhishek@gmail.com>
2021-01-26 19:27:17 -0500

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('Test Initial Color of Button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const ColorButton = screen.getByRole('button', {name: 'Click to change color to blue'});
+  expect(ColorButton).toHaveStyle({backgroundColor: 'red'});
 });
+
+
+test('Test Button Color as Blue after Click', () => {
+  render(<App />);
+  const ColorButton = screen.getByRole('button', {name: 'Click to change color to blue'});
+  fireEvent.click(ColorButton);
+  expect(ColorButton).toHaveStyle({backgroundColor: 'blue'});
+});
+
