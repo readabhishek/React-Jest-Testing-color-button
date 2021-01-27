8b2cf8d97a4ef1463ace9c8a2776515a43b7fe6e
Abhishek Kumar <readabhishek@gmail.com>
2021-01-27 14:19:17 -0500

    Updated 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 
+
 function App() {
 
     const [btnColor, setBtnColor] = useState('red');
-    const newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+    const [oldBtnColor, setOldBtnColor] = useState('red');
     const [checked, setChecked] = useState(false);
 
+    let newBtnColor = btnColor === 'gray' ? (oldBtnColor === 'red'? 'blue' : 'red') : (btnColor === 'red' ? 'blue' : 'red');
 
-    function setCheckedValue() {
-        let val = document.getElementById('chk1').checked;
-        setChecked(val);
-        //console.log("CheckBox Checked Element: ", val);
-    }
 
-    useEffect( () => {       /* This is React hook, combination of ComponentDidMount and ComponentDidUpdate   */
-        //console.log("CheckBox Checked State: ", checked);
-    })
+    /* This is React hook, combination of ComponentDidMount and ComponentDidUpdate. This is used
+    * because the state change is async in useState hook. This ensures that the updated state is available.   */
+    useEffect( () => {
+        if (checked === true) {
+            setOldBtnColor(btnColor);
+            setBtnColor('gray');
+        } else {
+            if (btnColor === 'gray') {
+                setBtnColor(oldBtnColor);
+                newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+            } else {
+                setOldBtnColor(btnColor);
+                newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+            }
+        }
+
+    }, [checked])
+
+
 
     return (
+
         <div id='mainDiv' className="content">
             <button id='btn' style={{backgroundColor: btnColor}}
                     onClick={() => {setBtnColor(newBtnColor)}} disabled={checked}>Click to change color to {newBtnColor} </button>
             <div className="check-box">
                 <input
-                    type="checkbox" id="chk1" name="chk1" value="false" onChange={() => {setCheckedValue()}}/>
+                    type="checkbox" id="chk1" name="chk1" value="false" onChange={(e) => {setChecked(e.target.checked)}}/>
                 <label htmlFor="chk1">Disable Button</label>
             </div>
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,18 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 
+
+/* 1.   */
+
 test('Test Initial Color of Button', () => {
   render(<App />);
   const ColorButton = screen.getByRole('button', {name: 'Click to change color to blue'});
   expect(ColorButton).toHaveStyle({backgroundColor: 'red'});
 });
 
+
+
+/* 2.  */
 
 test('Test Button Color as Blue after Click', () => {
   render(<App />);
@@ -17,7 +23,9 @@ test('Test Button Color as Blue after Click', () => {
 });
 
 
-/* Script for Check box and Button initial condition */
+
+/* 3.  Script for Check box and Button initial condition */
+
 test("Check the initial conditions for Check box", () => {
 
   /* Checks that button is enabled initially */
@@ -33,7 +41,8 @@ test("Check the initial conditions for Check box", () => {
 
 
 
-/* Script to test if button enables and disables  */
+/* 4. Script to test if button enables and disables  */
+
 test("Check if button disables after check-box checked and vice-versa", () => {
   render(<App />);
 
@@ -52,3 +61,36 @@ test("Check if button disables after check-box checked and vice-versa", () => {
   expect(colorButton).not.toBeDisabled();
 
 });
+
+
+
+
+/* 5. Script to test if button turns gray after disable */
+
+test("Check if button turns gray after disable and vice versa", () => {
+  render(<App />);
+  const checkBox = screen.getByRole('checkbox', {name: 'Disable Button'});
+  const colorButton = screen.getByRole('button', {name: 'Click to change color to blue'});
+
+  /* default not gray */
+  expect(colorButton).not.toHaveStyle({backgroundColor: 'gray'});
+
+  /* FireEvents - Make checkbox enable */
+  fireEvent.click(checkBox);
+
+  expect(checkBox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+  expect(colorButton).toHaveStyle({backgroundColor: 'gray'});
+
+  /* Now we'll make the checkbox un-checked and see if the buttons is enabled  */
+  fireEvent.click(checkBox)  // Un-check the checkbox
+  expect(checkBox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
+
+  /* Below code checks if the button goes back to red or blue color after being enabled */
+  expect(colorButton).toHaveStyle({backgroundColor: 'red' || 'blue'});
+  expect(colorButton).not.toHaveStyle({backgroundColor: 'gray'});
+
+});
+
+
